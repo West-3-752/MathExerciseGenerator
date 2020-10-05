6247db63b3810b05d8774c536dfd7e5d1e43adf0
gc922 <57403499+gc922@users.noreply.github.com>
2020-10-05 23:48:30 +0800

    update

--- 四则运算生成器/src/MathExerciseGenerator/Main.java
+++ 四则运算生成器/src/MathExerciseGenerator/Main.java
@@ -1,13 +1,111 @@
 package MathExerciseGenerator;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * @author GC
  */
 public class Main{
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
         ReadWriteTxt readWriteTxt = new ReadWriteTxt();
-        readWriteTxt.creatTxt("./Exercises.txt");
+        Creator creator = new Creator();
+        System.out.println("四则运算生成器");
+        System.out.print("请输入生成题目的数量(1~10000)：");
+        int n = scanner.nextInt();
+        System.out.print("请输入题目的数值范围：");
+        int r = scanner.nextInt();
+        /*生成练习题数组*/
+        String[][] exercises = creator.createExercise(n,r);
+        //在执行目录下生成练习题文本以及答案文本
+        readWriteTxt.createTxt("D:\\1.txt");
+        readWriteTxt.createTxt("D:\\2.txt");
+        //将练习题和答案分别存入两个文本中
+        for (int i = 0; i < n; i++){
+            String j =String.valueOf(i+1);
+            readWriteTxt.writeTxt("D:\\2.txt", j + "." + " ");
+            readWriteTxt.writeTxt("D:\\2.txt", exercises[i][0]);
+            readWriteTxt.writeTxt("D:\\2.txt","\n");
+            readWriteTxt.writeTxt("D:\\1.txt", j + "." + " ");
+            readWriteTxt.writeTxt("D:\\1.txt", exercises[i][1]);
+            readWriteTxt.writeTxt("D:\\1.txt","\n");
+        }
+        System.out.println("输入题目路径与答案路径判定对错并统计");
+        String path1 = scanner.nextLine();
+        String path2 = scanner.nextLine();
+        String[][] correctWrong = compareAnswer(path1, path2);
+        String correct = exchange(correctWrong[0]);
+        String wrong = exchange(correctWrong[1]);
+        readWriteTxt.createTxt("D:\\3.txt");
+        readWriteTxt.writeTxt("D:\\3.txt", "Correct：" + correctWrong[0].length + "（" + correct);
+        readWriteTxt.writeTxt("D:\\3.txt","\n");
+        readWriteTxt.writeTxt("D:\\3.txt", "Wrong：" + correctWrong[1].length + "（" + wrong);
+    }
+
+    /**
+     * 答案比较函数
+     * @param path1 题目文件的路径
+     * @param path2 答案文件的路径
+     * @return correctWrong[2][]，[0]为对的题目序号数组 ，[1]为错的题目序号数组
+     * @throws IOException
+     */
+    public static String[][] compareAnswer(String path1, String path2) throws IOException {
+        ReadWriteTxt readWriteTxt = new ReadWriteTxt();
+        Creator creator = new Creator();
+        String[] exercise = readWriteTxt.readTxt(path1);
+        String[] answer = readWriteTxt.readTxt(path2);
+        exercise = select(exercise);
+        answer = select(answer);
+        String[][] exerciseArray = new String[exercise.length][2];
+        for (int i = 0; i < exercise.length; i++){
+            exerciseArray[i][0] = exercise[i];
+            exerciseArray[i][1] = answer[i];
+        }
+        Boolean[] booleans = creator.correctAnswer(exerciseArray);
+        String[][] correctWrong = new String[2][];
+        int i = 0;
+        int correctLength = 0;
+        int wrongLength = 0;
+        while (i < booleans.length){
+
+            if (booleans[i] == true){
+                correctWrong[0][correctLength] = String.valueOf(i);
+                correctLength++;
+            }
+            else {
+                correctWrong[1][wrongLength] = String.valueOf(i);
+                wrongLength++;
+            }
+            i++;
+        }
+        return correctWrong;
+    }
+
+    /**
+     * 题目序号数组规范输出函数
+     * @param strings 题目序号数组
+     * @return s 规范后的字符串
+     */
+    public static String exchange(String[] strings){
+        String s = null;
+        for (int i = 0; i < strings.length; i++){
+            s = s + strings[i] + "，";
+        }
+        s+= "）";
+        return s;
+    }
+
+    /**
+     * 题目序号过滤函数
+     * @param strings 需要过滤的字符串数组
+     * @return 过滤完的字符串数组
+     */
+    public static String[] select(String[] strings){
+        String[] selectArray = new String[strings.length];
+        for (int i = 0; i < strings.length; i++){
+            selectArray[i] = strings[i].substring(strings[i].indexOf(" ")+1);
+        }
+        return selectArray;
     }
 }
--- 四则运算生成器/src/MathExerciseGenerator/ReadWriteTxt.java
+++ 四则运算生成器/src/MathExerciseGenerator/ReadWriteTxt.java
@@ -1,6 +1,8 @@
 package MathExerciseGenerator;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author GC
@@ -11,33 +13,45 @@ public class ReadWriteTxt {
      * @param path 路径
      * @throws IOException
      */
-    public void creatTxt(String path) throws IOException {
+    public void createTxt(String path) throws IOException {
         File file = new File(path);
         if (!file.exists()){
             file.getParentFile().mkdirs();
         }
         file.createNewFile();
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write("");
+        fileWriter.close();
     }
 
     /**
      * 读取文本函数
      * @param path 路径
-     * @return 返回读取的字符串
+     * @return 返回读取的字符串数组
      * @throws IOException
      */
-    public String readTxt(String path) throws IOException {
+    public String[] readTxt(String path) throws IOException {
         File file = new File(path);
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        StringBuffer stringBuffer = null;
-        String str = null;
+        List<String>list = new ArrayList<>();
+        String str;
         while ((str = bufferedReader.readLine())!=null) {
-            stringBuffer.append(str);
+            list.add(str);
         }
-        String string = new String(stringBuffer);
-        fileReader.close();
+        Object[] array = list.toArray();
+        String[] stringArray = new String[list.size()];
+        int i = 0;
+        for (Object o: array){
+            if(o instanceof String) {
+                //对每个元素进行类型判断
+                stringArray[i] = (String)o;
+            }
+            i++;
+        }
         bufferedReader.close();
-        return string;
+        fileReader.close();
+        return stringArray;
     }
 
     /**
@@ -49,10 +63,11 @@ public class ReadWriteTxt {
 
     public void writeTxt(String path,String date) throws IOException{
         File file = new File(path);
-        FileWriter fileWriter = new FileWriter(file);
+        FileWriter fileWriter = new FileWriter(file,true);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         bufferedWriter.write(date);
-        fileWriter.close();
         bufferedWriter.close();
+        fileWriter.close();
     }
+
 }

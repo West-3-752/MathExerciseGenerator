830fbedc97fd1f93a0c3a12a2b5c711090054b5c
0Oo-oO0 <46864199+0Oo-oO0@users.noreply.github.com>
2020-10-08 19:56:09 +0800

    1.0.0

--- 四则运算生成器/src/MathExerciseGenerator/ReadWriteTxt.java
+++ 四则运算生成器/src/MathExerciseGenerator/ReadWriteTxt.java
@@ -10,12 +10,13 @@ import java.util.List;
 public class ReadWriteTxt {
     /**
      * 创建txt文档函数
+     *
      * @param path 路径
      * @throws IOException
      */
     public void createTxt(String path) throws IOException {
         File file = new File(path);
-        if (!file.exists()){
+        if (!file.exists()) {
             file.getParentFile().mkdirs();
         }
         file.createNewFile();
@@ -26,6 +27,7 @@ public class ReadWriteTxt {
 
     /**
      * 读取文本函数
+     *
      * @param path 路径
      * @return 返回读取的字符串数组
      * @throws IOException
@@ -34,18 +36,18 @@ public class ReadWriteTxt {
         File file = new File(path);
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        List<String>list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         String str;
-        while ((str = bufferedReader.readLine())!=null) {
+        while ((str = bufferedReader.readLine()) != null) {
             list.add(str);
         }
         Object[] array = list.toArray();
         String[] stringArray = new String[list.size()];
         int i = 0;
-        for (Object o: array){
-            if(o instanceof String) {
+        for (Object o : array) {
+            if (o instanceof String) {
                 //对每个元素进行类型判断
-                stringArray[i] = (String)o;
+                stringArray[i] = (String) o;
             }
             i++;
         }
@@ -56,14 +58,15 @@ public class ReadWriteTxt {
 
     /**
      * 写入文本函数
+     *
      * @param path 路径
      * @param date 写入得到数据
      * @throws IOException
      */
 
-    public void writeTxt(String path,String date) throws IOException{
+    public void writeTxt(String path, String date) throws IOException {
         File file = new File(path);
-        FileWriter fileWriter = new FileWriter(file,true);
+        FileWriter fileWriter = new FileWriter(file, true);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         bufferedWriter.write(date);
         bufferedWriter.close();
